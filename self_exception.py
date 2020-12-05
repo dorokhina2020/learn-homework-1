b76f8b8e97e09de58e50d8c3000931b2b81d8271
dorokhina2020 <pocorubato@gmailcom>
2020-12-05 11:56:20 +0300

    исключила из git трен файлы

--- self_exception.py
+++
@@ -1,8 +0,0 @@
-def cut_cake(people):
-    try:
-        part = 1 / people
-        print(f'Каждый человек получит {part} пирога')
-    except (TypeError, ZeroDivisionError):
-        print('Проверь ввод! Я умею делить только на число отличное от 0')
-
-cut_cake(0)
